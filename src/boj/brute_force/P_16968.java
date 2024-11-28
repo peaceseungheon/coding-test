0090818ca418f05f22ab99108a20f5e7f12c1f14
seungheon <77201925+peaceseungheon@users.noreply.github.com>
2024-11-28 23:24:04 +0900

    코딩 테스트 문제 풀이
    백준 차량번호판 구하기 16968 (브루트포스)

---
+++ src/boj/brute_force/P_16968.java
@@ -0,0 +1,45 @@
+package boj.brute_force;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class P_16968 {
+
+    static int count = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String type = br.readLine();
+        // 출력: 가능한 차량 번호판의 개수
+        // 로직: 1. 형식(dd, dc, cc, cd)에 따라 번호판의 개수를 구한다.
+        // 2-1 : 나올 수 있는
+        makeCarNumber(type.toCharArray(), 0, "");
+        System.out.println(count);
+    }
+
+    static void makeCarNumber(char[] type, int typeIndex, String str){
+        if(typeIndex == type.length){
+            count++;
+            return;
+        }
+        String last = null;
+        if(!str.isEmpty()){
+            last = str.substring(str.length()-1);
+        }
+        if(type[typeIndex] == 'c'){
+            for(char c = 'a'; c <= 'z'; c++){
+                if(String.valueOf(c).equals(last)) continue;
+                makeCarNumber(type, typeIndex+1, str + c);
+            }
+        }else{
+            for(int i = 0; i < 10; i++){
+                if(String.valueOf(i).equals(last)) continue;
+                makeCarNumber(type, typeIndex+1, str+i);
+            }
+        }
+    }
+}
